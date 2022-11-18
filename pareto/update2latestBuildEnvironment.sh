d1ae0431e3647daf166fbe57781a52bc4e99f264
corno <corno.astn.a@schraverus.com>
2022-11-18 19:55:57 +0100

    update buildenvironment

--- pareto/update2latestBuildEnvironment.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-scriptDir=`realpath $(dirname "$0")`
-
-npx npm-check-updates -u --packageFile "$scriptDir/package.json" && \
-npx npm-safe-install -t "$scriptDir"
-
-"$scriptDir/node_modules/pareto-buildenvironment/initialize.sh" "$scriptDir/.."
